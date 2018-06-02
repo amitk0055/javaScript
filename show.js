c152bcb52cc5d2c2205b416d45904578fce3cb4f
amit kumar <amitk0055@gmail.com>
2018-06-02 22:05:59 +0530

    hello

---
+++ show.js
@@ -0,0 +1,5 @@
+var data='{ "firstName":"Amit" ,"lastName":"Kumar", "mob":"9006509030","place":"Patna"}';
+var text = '{ "name":"John", "birth":"1986-12-14", "city":"New York"}';
+
+var addData=JSON.parse(data);
+console.log(addData.lastName);
