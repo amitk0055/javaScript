28a5edeef83c3c3ff55aed7b958a70c30333514c
Amit <38669440+amitk0055@users.noreply.github.com>
2018-06-21 18:06:56 +0530

    Add files via upload

---
+++ online exam/js/script.js
@@ -0,0 +1,9 @@
+
+var currentTime=new Date().getTime();
+console.log(currentTime);
+function setTimer(){
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+} 
+
